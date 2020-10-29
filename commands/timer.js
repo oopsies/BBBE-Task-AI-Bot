8d1cffd6068298cdf298f82c0664b8f8e823b5eb
kedwards71 <61069606+kedwards71@users.noreply.github.com>
2020-10-29 12:12:57 -0500

    u

--- commands/timer.js
+++ commands/timer.js
@@ -6,22 +6,10 @@ module.exports = {
         let Timer = args[0]
         if(args.length===0)
         {
-            return message.channel.send("Usage: !timer + duration + s|m|h\n(ex. !timer 30s )\n or\n Usage: !timer + duration + s|m|h + command\n(ex. !timer 30s !ping)\n or \nUsage: !timer + duration + s|m|h + command + target\n(!timer 30s !encour @target)")
+            return message.channel.send("Usage: !timer + duration + s|m|h\n(ex. !timer 30s )\n")
         }
         else if(args.length===1)
         {
-            message.channel.send("Timer:"+ms(ms(Timer),{long:true}))
-            setTimeout(function(){
-                message.channel.send(message.author.toString()+` It has been:${ms(ms(Timer),{long:true})}`)
-            },ms(Timer))
-        }
-        else if(args.length>=2)
-        {
-            arr = []
-            arr.forEach(myfunction2)
-            function myfunction2(item){
-                message.channel.send(`${item}`)
-            }
             message.channel.send("Timer:"+ms(ms(Timer),{long:true}))
             setTimeout(function(){
                 message.channel.send(message.author.toString()+` It has been:${ms(ms(Timer),{long:true})}`)
