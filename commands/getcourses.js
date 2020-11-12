19a3d67fff9c69de16c26a611b215a0ca3edc409
Jordan Black <jordan.black213@gmail.com>
2020-11-12 11:03:12 -0600

    added pagination to courses

--- commands/getcourses.js
+++ commands/getcourses.js
@@ -3,6 +3,7 @@ const helper = require ('../helper.js');
 const https = require('https');
 const { DiscordAPIError } = require('discord.js');
 const Discord = require('discord.js');
+const Pagination = require('discord-paginationembed');
 
 module.exports = {
     name: 'getcourses',
@@ -35,23 +36,47 @@ module.exports = {
                     var dat = JSON.parse(d);
                     //console.log(dat);
                     //print name of courses if they are defined
-                    const embed = new Discord.MessageEmbed()
-                    .setColor('#059033')
-                    .setTitle('Future Assignments')
-                   // .setURL() ~ Insert Canvas Dashboard Page Here?
-                    .setThumbnail('https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/North_Texas_Mean_Green_logo.svg/1200px-North_Texas_Mean_Green_logo.svg.png');
+                    //const embed = new Discord.MessageEmbed()
+                    //.setColor('#059033')
+                    //.setTitle('Future Assignments')
+                    //.setURL() ~ Insert Canvas Dashboard Page Here?
+                    //.setThumbnail('https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/North_Texas_Mean_Green_logo.svg/1200px-North_Texas_Mean_Green_logo.svg.png');
                     
+                   // const FieldsEmbed = new Pagination.FieldsEmbed();
                     
+                    const embeds = [];
+ 
                     var countCourses = 1
                     for (var i = 0; i < dat.length; i++){
                         if (dat[i].name != undefined) {
                             console.log(dat[i].name);
                             //message.channel.send(dat[i].name + "\n");
-                            embed.addField(`Course ${countCourses}`, dat[i].name, false);
+                            //embed.addField(`Course ${countCourses}`, dat[i].name, false);
+                            embeds.push(new Discord.MessageEmbed().addField(`Course ${countCourses}`, dat[i].name, false));
                             countCourses++; 
                         }
                     }
-                    message.channel.send(embed);
+
+                    const myImage = message.author.displayAvatarURL();
+
+                    new Pagination.Embeds()
+                        .setArray(embeds)
+                        .setAuthorizedUsers([message.author.id])
+                        .setChannel(message.channel)
+                        .setPageIndicator(true)
+                        .setPage(3)
+                        // Methods below are for customising all embeds
+                        .setImage(myImage)
+                        .setThumbnail('https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/North_Texas_Mean_Green_logo.svg/1200px-North_Texas_Mean_Green_logo.svg.png')
+                        .setTitle('Fall 2020 Courses')
+                        .setDescription(`Here are the coures for: ${message.author}`)
+                        //.setFooter('Test Footer Text')
+                        //.setURL(myImage)
+                        .setColor('#059033')
+                        //.addField('\u200b', '\u200b')
+                        //.addField('Test Field 1', 'Test Field 1', true)
+                        //.addField('Test Field 2', 'Test Field 2', true)
+                        .build();
                 });
               
               }).on('error', (e) => {
@@ -60,5 +85,6 @@ module.exports = {
 
             return;
         }
+       
     }
 };
