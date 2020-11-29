63c12d27a361d449b8fabdb6e2ced61b37c1b12f
Jordan Black <jordan.black213@gmail.com>
2020-11-29 15:07:04 -0600

    added pagniation to grades, assignments, assignment grades

--- commands/getgrades.js
+++ commands/getgrades.js
@@ -3,6 +3,7 @@ const helper = require('../helper.js');
 const fs = require('fs');
 const { DiscordAPIError } = require('discord.js');
 const Discord = require('discord.js');
+const Pagination = require('discord-paginationembed');
 
 
 var prefix = "https://canvas.instructure.com"
@@ -43,13 +44,7 @@ module.exports = {
                 //console.log(result)
 
                 //set up embed
-                const embed = new Discord.MessageEmbed()
-                .setColor('#059033')
-                .setTitle('Your grades')
-                //.setURL() ~ Insert Canvas Dashboard Page Here?
-                .setThumbnail('https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/North_Texas_Mean_Green_logo.svg/1200px-North_Texas_Mean_Green_logo.svg.png')
-                .setFooter("Wrong courses? Use !update to refresh your courses.", "https://e7.pngegg.com/pngimages/1017/780/png-clipart-exclamation-mark-exclamation-mark.png")
-                .setTimestamp();
+                const embeds = [];
 
                 for (let i = 0; i < courseIDs.length; i++){ //for each course, go through the enrollments and find the correct one
                     for (let j = 0; j < result.length; j++){
@@ -58,11 +53,13 @@ module.exports = {
                             let grade = result[j].grades.current_score;
 
                             if (grade == null || grade == undefined){
-                                embed.addField(`${courseNames[i]}`, "Your instructor has not put in a total grade for this course.", false);
+                                //embed.addField(`${courseNames[i]}`, "Your instructor has not put in a total grade for this course.", false);
+                                embeds.push(new Discord.MessageEmbed().addField(`${courseNames[i]}`, "Your instructor has not put in a total grade for this course.", false));
                                 break;
                             }
                             else {
-                                embed.addField(`${courseNames[i]}`, `${grade}%`, false);
+                                //embed.addField(`${courseNames[i]}`, `${grade}%`, false);
+                                embeds.push(new Discord.MessageEmbed().addField(`${courseNames[i]}`, `${grade}%`, false));
                                 break;
                             }
 
@@ -70,7 +67,24 @@ module.exports = {
                     }
                 }
 
-                return message.channel.send(embed);
+                const myImage = message.author.displayAvatarURL();
+
+                new Pagination.Embeds()
+                    .setArray(embeds)
+                    .setAuthorizedUsers([message.author.id])
+                    .setChannel(message.channel)
+                    .setPageIndicator(true)
+                    .setPage(1)
+                    // Methods below are for customising all embeds
+                    .setImage(myImage)
+                    .setThumbnail('https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/North_Texas_Mean_Green_logo.svg/1200px-North_Texas_Mean_Green_logo.svg.png')
+                    .setTitle('Your Grades')
+                    .setDescription(`Here are the grades for: ${message.author}`)
+                    .setThumbnail('https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/North_Texas_Mean_Green_logo.svg/1200px-North_Texas_Mean_Green_logo.svg.png')
+                    //.setFooter("Wrong courses? Use !update to refresh your courses.", "https://e7.pngegg.com/pngimages/1017/780/png-clipart-exclamation-mark-exclamation-mark.png")
+                    .setTimestamp()
+                    .setColor('#059033')
+                    .build();    
 
             });
             return;
